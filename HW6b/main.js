fc6c624bbc05822142aa7e19efec6195cfc90e52
theom-l <85464878+theom-l@users.noreply.github.com>
2021-11-08 02:21:31 -0500

    update main.js for cart

--- HW6b/main.js
+++ HW6b/main.js
@@ -1,5 +1,4 @@
 
-
 let cart_counter = 0
 
 /* Page update functions when clicking radio buttons */
@@ -108,6 +107,41 @@ function AddToCart() {
     }
     setTimeout(ChangeBack, 1500)
 }
+
+function displayCart () { 
+
+    let cart = []
+    if (!localStorage.getItem("savedcart")) { //checks if it exists yet
+        localStorage.setItem("savedcart", JSON.stringify([])) 
+     }
+     else {
+         cart = JSON.parse(localStorage.getItem('savedcart'))
+     }
+
+    const cartDiv = document.getElementById("cart_page_container");
+    const template = document.getElementById("cart-item-template");
+
+
+    function showProductInCart(cart_item) {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector('.cart_flavor').innerText = cart_item.flavor;
+        clone.querySelector('.cart_glaze').innerText = cart_item.glaze;
+        clone.querySelector('.cart_qty').innerText = cart_item.qty;
+        clone.querySelector('.cart_price').innerText = cart_item.price;
+        cartDiv.appendChild(clone);
+    }
+
+    for (let i = 0; i < cart.length; i++ ) {
+        let cart_item = cart[i]
+        showProductInCart (cart_item)
+    }
+}
+
+function RemoveFromCart () {
+
+    this.parentElement.remove()
+
+}
 /* 
  */
 
@@ -116,9 +150,6 @@ function AddToCart() {
 
 
 
-
-
-/* 
 
 const sugarmilk  = document.getElementById('sugar-milk');
 sugarmilk.addEventListener('click', function() {
